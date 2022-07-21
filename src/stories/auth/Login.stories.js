b07bb704aeae6bf086b3ff14a7ce90a12a8d8873
Chris Schillinger <chris@koptional.com>
2022-07-20 21:11:29 -0500

    feat: login + signup stories

---
+++ src/stories/auth/Login.stories.js
@@ -0,0 +1,30 @@
+import Login from '../../lib/auth/Login.svelte';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes
+export default {
+	title: 'Auth/Login',
+	component: Login,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+		label: { control: 'text' },
+		onClick: { action: 'onClick' },
+		primary: { control: 'boolean' },
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large']
+		}
+	}
+};
+
+// More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args
+const Template = (args) => ({
+	Component: Login,
+	props: args,
+	on: {
+		click: args.onClick
+	}
+});
+
+// More on args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Primary = Template.bind({});
---
+++ src/stories/auth/Signup.stories.js
@@ -0,0 +1,30 @@
+import Signup from '../../lib/auth/Signup.svelte';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes
+export default {
+	title: 'Auth/Signup',
+	component: Signup,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+		label: { control: 'text' },
+		onClick: { action: 'onClick' },
+		primary: { control: 'boolean' },
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large']
+		}
+	}
+};
+
+// More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args
+const Template = (args) => ({
+	Component: Signup,
+	props: args,
+	on: {
+		click: args.onClick
+	}
+});
+
+// More on args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Primary = Template.bind({});
