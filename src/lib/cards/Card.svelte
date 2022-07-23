44adeaa51a22acd87b9785d86e10e10d9d1e0dba
Chris Schillinger <cschillinger1994@gmail.com>
2022-07-22 21:34:46 -0500

    feat: /flash endpoint and Card update ui

--- src/lib/cards/Card.svelte
+++ src/lib/cards/Card.svelte
@@ -15,15 +15,15 @@ import { fade } from "svelte/transition";
         onComplete()
     }
 </script>
-<div class="bg-white">
-    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">  
-      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-6">
+
+   
+  
           {#if show}
-          <button on:click={transitionCard} class="group">
+          <button  on:click={transitionCard} class="group w-80  rounded overflow-hidden shadow-lg">
             <div class="w-full aspect-w-5 aspect-h-5 bg-gray-200 rounded-t-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img src={phrase.image} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                <img src={phrase.image} alt="" class="w-full h-96 object-center object-cover group-hover:opacity-75">
             </div>
-            <div class={`${show ? "bg-blue-400" : "bg-yellow-200"} rounded-b-lg h-24 flex flex-col justify-center items-center`}>
+            <div class={`bg-rose-400 rounded-b-lg h-24 flex flex-col justify-center items-center`}>
                 <h3 class=" text-2xl font-extrabold text-gray-700">{phrase.original}</h3>
                 {#if show}
                 <h3 class=" text-xl  text-gray-200">{phrase.translation}</h3>
@@ -31,11 +31,11 @@ import { fade } from "svelte/transition";
             </div>
             </button>
           {:else}
-          <button in:fade="{{duration: 1000}}" on:click={showTranslation} class="group">
+          <button in:fade="{{duration: 750}}" on:click={showTranslation} class="group w-80 rounded overflow-hidden shadow-lg">
             <div class="w-full aspect-w-5 aspect-h-5 bg-gray-200 rounded-t-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img src={phrase.image} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                <img src={phrase.image} alt="" class="w-full  h-96 object-center object-cover group-hover:opacity-75">
             </div>
-            <div class={`${show ? "bg-blue-400" : "bg-yellow-200"} rounded-b-lg h-24 flex flex-col justify-center items-center`}>
+            <div class={`bg-amber-400 rounded-b-lg h-24 flex flex-col justify-center items-center`}>
                 <h3 class=" text-2xl font-extrabold text-gray-700">{phrase.original}</h3>
                 {#if show}
                 <h3 class=" text-xl  text-gray-200">{phrase.translation}</h3>
@@ -43,7 +43,7 @@ import { fade } from "svelte/transition";
             </div>
             </button>
           {/if}
-        </div>
-    </div>
-  </div>
+      
+    
+ 
   
---
+++ src/routes/flash.svelte
@@ -0,0 +1,43 @@
+<script>
+	
+	import Card from '$lib/cards/Card.svelte';
+	
+	
+	let phrase = {
+		image: "https://images.unsplash.com/photo-1598974357801-cbca100e65d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80",
+		original: "Cavalo",
+		translation: "Horse"
+	}
+	const phrase2 = {
+		image: "https://media.istockphoto.com/photos/sloth-in-costa-rica-picture-id1068395160?k=20&m=1068395160&s=612x612&w=0&h=vn2GlQ1e9DwzYMra3tZ8adG_vA7IpZ3MLGdBhYuDQsU=",
+		original: "Preguiça",
+		translation: "Sloth"
+	}
+	let selectedPhrase = 0
+	let phrases = [
+		{
+		image: 'https://chriskresser.com/wp-content/uploads/raw-milk-1-e1563894986431.jpg',
+		original: 'Leite',
+		translation: 'Milk'
+	},
+	{
+		image: "https://images.unsplash.com/photo-1598974357801-cbca100e65d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80",
+		original: "Cavalo",
+		translation: "Horse"
+	},
+	{
+		image: "https://media.istockphoto.com/photos/sloth-in-costa-rica-picture-id1068395160?k=20&m=1068395160&s=612x612&w=0&h=vn2GlQ1e9DwzYMra3tZ8adG_vA7IpZ3MLGdBhYuDQsU=",
+		original: "Preguiça",
+		translation: "Sloth"
+	}
+	]
+	const cycleCard = () => selectedPhrase >= phrases.length -1 ? selectedPhrase = 0 : selectedPhrase = ++selectedPhrase
+</script>
+
+<div class="min-h-screen flex flex-col ">
+    <h1> Flash Cards</h1>
+	<div class="h-full flex flex-col justify-center items-center">
+        <Card  phrase={phrases[selectedPhrase]} onComplete={cycleCard}/>
+    </div>
+	
+</div>
