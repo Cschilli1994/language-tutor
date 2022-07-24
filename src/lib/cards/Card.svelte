aaefb810fed24a4d686ba0fb14c629d5401554e1
Chris Schillinger <cschillinger1994@gmail.com>
2022-07-24 14:56:47 -0500

    fix: remove card fade on hover

--- src/lib/cards/Card.svelte
+++ src/lib/cards/Card.svelte
@@ -23,7 +23,7 @@ import { fade } from "svelte/transition";
           {#if show}
           <button  on:click={transitionCard}   class="group w-80  rounded-3xl overflow-hidden shadow-lg border-white border-8">
             <div class="w-full aspect-w-5 aspect-h-5 bg-gray-200 rounded-t-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img src={phrase.image} alt="" class="w-full h-96 object-center object-cover group-hover:opacity-75">
+                <img src={phrase.image} alt="" class="w-full h-96 object-center object-cover ">
             </div>
             <div class={`bg-rose-400 rounded-b-lg h-24 flex flex-col justify-center items-center`}>
                 <h3 class=" text-2xl font-extrabold text-gray-700">{phrase[translation]}</h3>
@@ -35,7 +35,7 @@ import { fade } from "svelte/transition";
           {:else}
           <button   on:click={showTranslation} class="group w-80 rounded-3xl overflow-hidden shadow-lg border-white border-8">
             <div class="w-full aspect-w-5 aspect-h-5 bg-gray-200 rounded-t-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img src={phrase.image} alt="" class="w-full  h-96 object-center object-cover group-hover:opacity-75">
+                <img src={phrase.image} alt="" class="w-full  h-96 object-center object-cover ">
             </div>
             <div class={`bg-amber-400 rounded-b-lg h-24 flex flex-col justify-center items-center`}>
                 <h3 class=" text-2xl font-extrabold text-gray-700">{phrase[original]}</h3>
